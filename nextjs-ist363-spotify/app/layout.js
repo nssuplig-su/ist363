c4a3a8227e7f3b017fa79edc4148a1608af07bc4
nssuplig-su <nssuplig@syr.edu>
2024-04-25 18:28:13 -0400

    attendance 04/25: working on spotify

---
+++ nextjs-ist363-spotify/app/layout.js
@@ -0,0 +1,15 @@
+import "../sass/global.scss";
+
+export const metadata = {
+  title: "IST 363 Spotify",
+  description:
+    "A marketing page for Spotify spotlighting the top 10 artists of 2024.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ nextjs-ist363-spotify/app/page.js
@@ -0,0 +1,12 @@
+import Heading from "../components/Heading";
+
+const Home = () => {
+  return (
+    <main>
+      <Heading level={1}>Spotify</Heading>
+      <Heading level={2}>Artists</Heading>
+      <Heading level={3}>Songs</Heading>
+    </main>
+  );
+};
+export default Home;
